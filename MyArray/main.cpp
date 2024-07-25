30f4973a2bc6369352f4e5e38f1bc9a2ab2d7aad
trustedhost <mrchjung@korea.ac.kr>
2024-07-25 09:39:42 +0900

    MyArray : Array 를 순차탐색하기 위한 Iterator class 를 추가.

--- MyArray/main.cpp
+++ MyArray/main.cpp
@@ -22,6 +22,20 @@ private:
     Address* top;
 
 public:
+    class Iterator {
+        int* location;
+        Array* arr;
+        friend Int;
+    public:
+        Iterator(Array* arr, int* loc = NULL);
+        Iterator(const Iterator& itr);
+        ~Iterator();
+        Iterator& operator++();
+        Iterator operator++(int);
+        Iterator& operator=(const Iterator& itr);
+        bool operator!=(const Iterator& itr);
+        Int operator*();
+    };
     Array(int dim, int* array_size);
     Array(const Array& arr);
     ~Array();
@@ -29,6 +43,8 @@ public:
     void copy_address(Address * dst, Address * src);
     void delete_address(Address* current);
     Int operator[](const int index);
+    Iterator begin();
+    Iterator end();
 
 };
 class Int {
@@ -114,6 +130,29 @@ Int Array::operator[](const int index) {
     return Int(index, 1, static_cast<void*>(top), this);
 }
 
+Array::Iterator Array::begin()
+{
+    int* arr = new int[dim];
+    for (int i = 0; i != dim; i++) arr[i] = 0;
+
+    Iterator temp(this, arr);
+    delete[] arr;
+
+    return temp;
+}
+
+Array::Iterator Array::end()
+{
+    int* arr = new int[dim];
+    arr[0] = size[0];
+    for (int i = 1; i < dim; i++) arr[i] = 0;
+
+    Iterator temp(this, arr);
+    delete[] arr;
+
+    return temp;
+}
+
 Int::Int(int index, int _level, void *_data, Array *_array) : level(_level), data(_data), array(_array) {
     if(level < 1 || index >= array->size[level - 1]) {
         data = NULL;
@@ -144,6 +183,70 @@ Int &Int::operator=(const int &a) {
     return *this;
 }
 
+Array::Iterator::Iterator(Array *arr, int *loc) : arr(arr) {
+    location = new int[arr->dim];
+    for (int i = 0; i < arr->dim; i++) {
+        location[i] = ( loc != NULL ? loc[i] : 0);
+    }
+}
+
+Array::Iterator::Iterator(const Iterator &itr) : arr(itr.arr) {
+    location = new int[arr->dim];
+    for (int i = 0; i != arr->dim; i++) location[i] = itr.location[i];
+}
+
+Array::Iterator::~Iterator() { delete[] location; }
+
+Array::Iterator &Array::Iterator::operator++() {
+    if(location[0] >= arr->size[0]) return(*this);
+    bool carry = false;
+    int i = arr->dim - 1;
+    do {
+        location[i]++;
+        if (location[i] >= arr->size[i] && i >= 1) {
+            location[i] -= arr->size[i];
+            carry = true;
+            i--;
+        } else {
+            carry = false;
+        }
+    } while (i >= 0 && carry);
+    return (*this);
+
+}
+Array::Iterator Array::Iterator::operator++(int) {
+    Iterator itr(*this);
+    ++(*this);
+    return itr;
+}
+
+Array::Iterator &Array::Iterator::operator=(const Iterator &itr)
+{
+    arr = itr.arr;
+    location = new int[itr.arr->dim];
+    for(int i = 0; i < arr->dim; i++) {
+        location[i] = itr.location[i];
+    }
+    return (*this);
+}
+
+bool Array::Iterator::operator!=(const Iterator &itr)
+{
+    if (itr.arr->dim != arr->dim) return true;
+    for (int i = 0; i < arr->dim; i++) {
+        if (location[i] != itr.location[i]) return true;
+    }
+    return false;
+}
+
+Int Array::Iterator::operator*()
+{
+    Int start = arr->operator[](location[0]);
+    for (int i = 1; i <= arr->dim - 1; i++) {
+        start = start.operator[](location[i]);
+    }
+    return start;
+}
 
 }
 
@@ -152,10 +255,15 @@ int main() {
     int size[] = {2, 3, 4};
     MyArray::Array arr(3, size);
 
+    MyArray::Array::Iterator itr = arr.begin();
+    for (int i = 0; itr != arr.end(); itr++, i++) (*itr) = i;
+    for (itr = arr.begin(); itr != arr.end(); itr++)
+        std::cout << *itr << std::endl;
+
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
             for (int k = 0; k < 4; k++) {
-                arr[i][j][k] = (i + 1) * (j + 1) * (k + 1);
+                arr[i][j][k] = (i + 1) * (j + 1) * (k + 1) + arr[i][j][k];
             }
         }
     }
@@ -167,5 +275,4 @@ int main() {
             }
         }
     }
-
 }
