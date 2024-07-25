02bcef9938dad3bd9f5983373b93178a751b3043
trustedhost <mrchjung@korea.ac.kr>
2024-07-25 12:07:55 +0900

    6-3. virtual 소멸자.
    부모 클래스의 소멸자에 virtual  키워드를 붙여서 메모리 누수 방지.

--- 6-3_Virtual_function_table/main.cpp
+++ 6-3_Virtual_function_table/main.cpp
@@ -3,7 +3,7 @@
 class Parent {
  public:
   Parent() { std::cout << "Parent 생성자 호출" << std::endl; }
-  ~Parent() { std::cout << "Parent 소멸자 호출" << std::endl; }
+  virtual ~Parent() { std::cout << "Parent 소멸자 호출" << std::endl; }
 };
 class Child : public Parent {
  public:
