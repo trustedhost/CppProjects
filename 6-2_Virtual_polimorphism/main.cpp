bbdf29f0cf3c0b3c766ad613ce74e13e79f41293
trustedhost <mrchjung@korea.ac.kr>
2024-07-25 11:50:15 +0900

    6-2. virtual
    virtual 키워드 사용 시 주의사항.
    1. 완전히 동일한 형태의 함수이어야 한다.
    2. virtual 키워드가 붙지 않은 함수에 override 를 할 수 없다.

--- 6-2_Virtual_polimorphism/main.cpp
+++ 6-2_Virtual_polimorphism/main.cpp
@@ -5,6 +5,7 @@ class Base {
 public:
     Base() : s("기반") { std::cout << "기반 클래스" << std::endl; }
     virtual void what() { std::cout << s << std::endl; }
+    //void printblabla() { std::cout << "blabla" << std::endl; };
 };
 
 class Derived : public Base {
@@ -12,6 +13,9 @@ class Derived : public Base {
 public:
     Derived() : s("파생"), Base() { std::cout << "파생 클래스" << std::endl; }
     void what() override { std::cout << s << std::endl; }
+    //void what() const override { std::cout << s << std::endl; } 함수의 형태가 완전히 동일해야 override 가능.
+    //void printblabla() override { std::cout << "bloblo" << std::endl; }; // virtual 함수에 대해서만 override 가능.
+
 };
 
 int main()
