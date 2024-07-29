a665783684aed72f7feb65ce3ccfe3e54abd64f3
trustedhost <mrchjung@korea.ac.kr>
2024-07-29 16:52:06 +0900

    9-4_template_meta_programming
    is_prime 구현.
    template 형식 안에 사칙연산 에러나는 경우

--- 9-4_template_meta_programming_3/main.cpp
+++ 9-4_template_meta_programming_3/main.cpp
@@ -1,34 +1,60 @@
 #include <iostream>
-//is_prime
-
-
-template <int N, int d>
-struct check_div {
-    static const bool result = (N % d == 0) || check_div<N,d + 1>::result;
-};
 
 template <int N>
-struct check_div<N, N /2> {
-    static const bool result = (N % (N / 2)) == 0;
+struct INT {
+    static const int num = N;
+};
+
+template <typename a, typename b>
+struct add {
+    typedef INT<a::num + b::num> result;
+};
+
+template <typename a, typename b>
+struct divide {
+    typedef INT<a::num / b::num> result;
+};
+
+using one = INT<1>;
+using two = INT<2>;
+using three = INT<3>;
+
+template <typename N, typename d>
+struct check_div {
+    // result 중에서 한 개라도 true 면 전체가 true
+    static const bool result = (N::num % d::num == 0) ||
+                               check_div<N, typename add<d, one>::result>::result;
+};
+
+template <typename N>
+struct _is_prime {
+    static const bool result = !check_div<N, two>::result;
+};
+
+template <>
+struct _is_prime<two> {
+    static const bool result = true;
+};
+
+template <>
+struct _is_prime<three> {
+    static const bool result = true;
+};
+
+template <typename N>
+struct check_div<N, typename divide<N, two>::result> {
+    static const bool result = (N::num % (N::num / 2) == 0);
 };
 
 template <int N>
 struct is_prime {
-    static const bool result = !check_div<N, 2>::result;
+    static const bool result = _is_prime<INT<N>>::result;
 };
-template <>
-struct is_prime<2> {
-    static const bool result = true;
-};
-template <>
-struct is_prime<3> {
-    static const bool result = true;
-};
-
 
 int main() {
-    using isPrime = is_prime<22>;
-    std::cout << isPrime::result << std::endl;
-
-    return 0;
+    std::cout << std::boolalpha;
+    std::cout << "Is 2 prime ? :: " << is_prime<2>::result << std::endl;
+    std::cout << "Is 10 prime ? :: " << is_prime<10>::result << std::endl;
+    std::cout << "Is 11 prime ? :: " << is_prime<11>::result << std::endl;
+    std::cout << "Is 61 prime ? :: " << is_prime<61>::result << std::endl;
 }
