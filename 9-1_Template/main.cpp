a385266f8597ccb7bca478329d8c109be324fd93
trustedhost <mrchjung@korea.ac.kr>
2024-07-29 11:59:38 +0900

    9-1_template Functor 의 개념
    functor = function 처럼 작동하는 class.
    장점 1. class 내부함수로 override 를 하는 것보다, functor  로 구성하는 것이 편리함.
    장점 2. 함수 포인터로 함수를 인자로 받는 것보다 functor  를 활용하는 것이 inline 으로 컴파일러가 최적화할 수 있다.

--- 9-1_Template/main.cpp
+++ 9-1_Template/main.cpp
@@ -53,19 +53,54 @@ void bubble_sort(Cont& cont) {
     }
 }
 
+template <typename Cont, typename Comp>
+void bubble_sort(Cont& cont, Comp& comp) {
+    for(int i = 0; i < cont.size(); i++) {
+        for(int j = i + 1; j < cont.size(); j++) {
+            if (!comp(cont[i], cont[j])) {
+                cont.swap(i,j);
+            }
+        }
+    }
+}
+
+struct Comp1 {
+    bool operator()(int a, int b) { return a > b ;}
+};
+struct Comp2 {
+    bool operator()(int a, int b) { return a < b ;}
+};
 
 
 int main() {
     Vector<int> int_vec;
-    int_vec.push_back(1);
     int_vec.push_back(3);
+    int_vec.push_back(1);
     int_vec.push_back(2);
-    int_vec.push_back(4);
+    int_vec.push_back(8);
+    int_vec.push_back(5);
+    int_vec.push_back(3);
 
-
-    bubble_sort(int_vec);
-    for(int i = 0; i < int_vec.size(); i++) {
-        std::cout << int_vec[i] << std::endl;
+    std::cout << "정렬 이전 ---- " << std::endl;
+    for (int i = 0; i < int_vec.size(); i++) {
+        std::cout << int_vec[i] << " ";
     }
 
+    Comp1 comp1;
+    bubble_sort(int_vec, comp1);
+
+    std::cout << std::endl << std::endl << "내림차순 정렬 이후 ---- " << std::endl;
+    for (int i = 0; i < int_vec.size(); i++) {
+        std::cout << int_vec[i] << " ";
+    }
+    std::cout << std::endl;
+
+    Comp2 comp2;
+    bubble_sort(int_vec, comp2);
+
+    std::cout << std::endl << "오름차순 정렬 이후 ---- " << std::endl;
+    for (int i = 0; i < int_vec.size(); i++) {
+        std::cout << int_vec[i] << " ";
+    }
+    std::cout << std::endl;
 }
