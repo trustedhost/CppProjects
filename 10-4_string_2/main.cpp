ff53fc3d2147ba557c1ec78b7ecbe62b8034b690
trustedhost <mrchjung@korea.ac.kr>
2024-07-31 12:09:00 +0900

    10-4_string_2
    string 인자를 받는 함수의 메모리 할당 문제.

---
+++ 10-4_string_2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+void* operator new(std::size_t count) {
+    std::cout << count << " bytes 할당 " << std::endl;
+    return malloc(count);
+}
+
+// 문자열에 "very" 라는 단어가 있으면 true 를 리턴함
+bool contains_very(const std::string& str) {
+    return str.find("very") != std::string::npos;
+}
+
+int main() {
+    // 암묵적으로 std::string 객체가 불필요하게 생성된다.
+    std::cout << std::boolalpha << contains_very("c++ string is very easy to use")
+              << std::endl;
+
+    std::cout << contains_very("c++ string is not easy to use") << std::endl;
+}
