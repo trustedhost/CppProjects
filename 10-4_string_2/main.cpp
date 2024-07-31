96757517f4a45a25955132e9e501e8dd6f3e6838
trustedhost <mrchjung@korea.ac.kr>
2024-07-31 13:12:17 +0900

    10-4_string_2
    string_view 는 반드시 메모리상에 존재하는 string 을 가리키고 있어야 한다.

--- 10-4_string_2/main.cpp
+++ 10-4_string_2/main.cpp
@@ -15,10 +15,29 @@ bool contains_very(std::string_view str) {
     return str.find("very") != std::string::npos;
 }
 
+std::string_view return_sv() {
+    std::string* s = new std::string("hello");
+    std::string_view s2 = *s;
+    return s2;
+}
+
 int main() {
     // 암묵적으로 std::string 객체가 불필요하게 생성된다.
     std::cout << std::boolalpha << contains_very("c++ string is very easy to use")
               << std::endl;
 
     std::cout << contains_very("c++ string is not easy to use") << std::endl;
+
+    //substr 의 경우
+    //string.substr 의 경우 string 을 return 		-> O(N)
+    //string_view.substr 의 경우 view 를 return  	-> O(1)
+
+    std::string s = "c++ string substring experiment";
+    std::cout << s.substr(0, s.find("string", 0)) << std::endl;
+
+    std::string_view s_view = "c++ string substring experiment";
+    std::cout << s.substr(0, s.find("string", 0)) << std::endl;
+
+    std::cout << return_sv() << std::endl;
+
 }
