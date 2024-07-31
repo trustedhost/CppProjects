6d1ae88092ec34d7588dd8b0229d77460697fc64
trustedhost <mrchjung@korea.ac.kr>
2024-07-31 17:10:15 +0900

    13-1_unique_ptr
    unique_ptr 을 vector 에 추가하려면, ptr 을 rvalue reference  로 바꾸기 위해 move 가 필요하다. ( vector 내에서 복사가 일어날 때 이동 생성자 호출하게 됨. )
    move 없이도 emplace_back을 통해서 완전 전달( perfect forwarding )을 할 수 있다.

--- 13-1_unique_ptr/main.cpp
+++ 13-1_unique_ptr/main.cpp
@@ -16,6 +16,10 @@ int main()
     std::unique_ptr<int> ptr(new int(1));
     vec.push_back(std::move(ptr));
     // vec.push_back(ptr);// 오류가 나게 됨. unique_ptr 은 단 하나의 포인터만 지목 가능.
+    vec.emplace_back(new int(2));
+
+    // int i = 3;
+    // vec.emplace_back(&i); // 오류가 나게 됨. unique_ptr 은 heap 동적 메모리를 관리하기 위해서 사용.
     print_vec(vec.begin(), vec.end());
     return 0;
 }
