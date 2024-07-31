d94a75f5ad548b4ba17a4e4ea024eb43250dac89
trustedhost <mrchjung@korea.ac.kr>
2024-07-31 15:58:23 +0900

    12-2_move_2
    클래스 안의 클래스 속 생생한 복사의 현장
    복사가 두 번 발생하는 처참한 현장

---
+++ 12-2_move_2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//객체 생성 시 이동 프로젝트
+
+class A {
+public:
+    A() {std::cout << "constructor" << std::endl;};
+    A(const A& a) {std::cout << "copy constructor" << std::endl;};
+    A(A&& a) {std::cout << "move constructor" << std::endl;};
+};
+
+class B {
+    A a_;
+public:
+    B(A a_) : a_(a_) {}
+};
+
+int main()
+{
+    A a;
+    std::cout << "create B" << std::endl;
+    B b(a);
+    return 0;
+}
