ff69e840ead67b5868edc66351a8b99178fffb10
trustedhost <mrchjung@korea.ac.kr>
2024-07-31 18:21:10 +0900

    13-2_shared_pointer
    weak_pointer 를 통해서 shared_pointer 가 순환참조하는 경우 scope를 벗어났음에도 shared 수가 살아있어서 객체가 소멸되지 않는 현상을 해결할 수 있다.

--- 13-2_shared_pointer/main.cpp
+++ 13-2_shared_pointer/main.cpp
@@ -3,9 +3,11 @@
 
 class A : public std::enable_shared_from_this<A> {
     int i;
+    std::weak_ptr<A> other;
 public:
-    A(int i = 1) {
+    A(int k = 1) {
         std::cout << "일반 생성자 호출!" <<  std::endl;
+        i = k;
     }
     A(const A& a) {
         std::cout << "복사 생성자 호출! " <<  std::endl;
@@ -13,6 +15,20 @@ public:
     ~A() = default;
     std::shared_ptr<A> get_shared_ptr() {
         return shared_from_this();
+    }
+    void set_other(std::weak_ptr<A> ptr) {
+        other = ptr;
+    }
+    void access_other() {
+        std::shared_ptr<A> o = other.lock();
+        if (o) {
+            std::cout << (o->get_number()) << std::endl;
+        } else {
+            std::cout << "it's gone.." << std::endl;
+        }
+    }
+    int get_number() {
+        return i;
     }
 };
 
@@ -25,8 +41,22 @@ int main()
     vec.push_back(ptr2); // shared_ptr 을 하나 복사하여 vec 에 저장. +1
     vec.push_back(std::move(ptr2));// 기존 ptr 을 없애고 이동하여 vec 에 저장 . +0
 
-
     std::cout << ptr.use_count() << std::endl;
 
+    std::vector<std::shared_ptr<A>> vec2;
+    vec2.push_back(std::shared_ptr<A>(new A(1)));
+    vec2.push_back(std::shared_ptr<A>(new A(2)));
+
+    std::cout << vec2[0]->get_number() << std::endl;
+    std::cout << vec2[1]->get_number() << std::endl;
+
+    vec2[0]->set_other(vec2[1]);
+    vec2[1]->set_other(vec2[0]);
+
+    vec2[0]->access_other();
+    vec2[1]->access_other();
+
+    vec2.pop_back();
+    vec2[0]->access_other();
 
 }
