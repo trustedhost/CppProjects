c8795d4dd9981fe8d1c52184ef624e3a2e2de94f
trustedhost <mrchjung@korea.ac.kr>
2024-07-31 16:11:34 +0900

    12-2_move_3
    wrapper 함수를 통해
    Template 에서 rvalue 다루기 뿌시기
    문제상황: wrapper 에서 rvalue 를 못받음.

---
+++ 12-2_move_3/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+
+
+template <typename T>
+void wrapper(T u) {
+    g(u);
+}
+
+class A{};
+
+void g(A& a) { std::cout << "좌측값 레퍼런스 호출" << std::endl; }
+void g(const A& a) { std::cout << "좌측값 상수 레퍼런스 호출" << std::endl; }
+void g(A&& a) { std::cout << "우측값 레퍼런스 호출" << std::endl; }
+
+int main()
+{
+    A a;
+    const A ca;
+
+    std::cout << "원본--------------------" << std::endl;
+    g(a);
+    g(ca);
+    g(A());
+    std::cout << "wrapper----------------" << std::endl;
+    wrapper(a);
+    wrapper(ca);
+    wrapper(A());
+
+// 원본--------------------
+// 좌측값 레퍼런스 호출
+// 좌측값 상수 레퍼런스 호출
+// 우측값 레퍼런스 호출
+// wrapper----------------
+// 좌측값 레퍼런스 호출
+// 좌측값 레퍼런스 호출
+// 좌측값 레퍼런스 호출
+    return 0;
+}
