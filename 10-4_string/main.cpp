cea3ad8e46f3f6f355f5126fa3bea95893a9d862
trustedhost <mrchjung@korea.ac.kr>
2024-07-31 11:18:38 +0900

    10-4_string
    객체 주소도 출력해서 비교.
    SSO 적용된 객체는 24바이트,
    적용안된 객체는 40바이트를 차지함

--- 10-4_string/main.cpp
+++ 10-4_string/main.cpp
@@ -15,12 +15,14 @@ int main() {
     std::string s1 = "this is a pretty long sentence!!!";
     std::cout << "s1 크기 : " << sizeof(s1) << std::endl;
     std::cout << "s1 내의 data : " << s1.data() << std::endl;
-    std::cout << "s1 내의 data : " << static_cast<void*>(s1.data()) << std::endl;
+    std::cout << "s1 내의 data주소 : " << static_cast<void*>(s1.data()) << std::endl;
+    std::cout << "s1 의 주소 : " << static_cast<void*>(&s1) << std::endl;
     std::cout << "s1 내의 string 길이 : " << s1.length() << std::endl;    std::cout << "s1 생성 --- " << std::endl;
     std::string s3 = "this is a pretty long sentence!!!";
     std::cout << "s3 크기 : " << sizeof(s3) << std::endl;
     std::cout << "s3 내의 data : " << s3.data() << std::endl;
     std::cout << "s3 내의 data : " << static_cast<void*>(s3.data()) << std::endl;
+    std::cout << "s3 의 주소 : " << static_cast<void*>(&s3) << std::endl;
     std::cout << "s3 내의 string 길이 : " << s3.length() << std::endl;
 
     std::cout << "s4 생성 --- " << std::endl;
@@ -28,12 +30,13 @@ int main() {
     std::cout << "s4 크기 : " << sizeof(s4) << std::endl;
     std::cout << "s4 내의 data : " << s4.data() << std::endl;
     std::cout << "s4 내의 data : " << static_cast<void*>(s4.data()) << std::endl;
+    std::cout << "s4 의 주소 : " << static_cast<void*>(&s4) << std::endl;
     std::cout << "s4 내의 string 길이 : " << s4.length() << std::endl;
-
     std::cout << "s2 생성 --- " << std::endl;
     std::string s2 = "short sentence";
     std::cout << "s2 크기 : " << sizeof(s2) << std::endl;
     std::cout << "s2 내의 data : " << s2.data() << std::endl;
     std::cout << "s2 내의 data : " << static_cast<void*>(s2.data()) << std::endl;
+    std::cout << "s2 의 주소 : " << static_cast<void*>(&s2) << std::endl;
     std::cout << "s2 내의 string 길이 : " << s2.length() << std::endl;
 }
