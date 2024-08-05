b0310c5ab11bd6fc944ee3c5e1cda40f9b9c3913
trustedhost <mrchjung@korea.ac.kr>
2024-08-05 15:09:34 +0900

    15-2_mutex
    2 개 mutex  사용 시 교착 상태 발생.

--- 15-2_mutex/main.cpp
+++ 15-2_mutex/main.cpp
@@ -5,24 +5,41 @@
 using std::thread;
 using std::vector;
 
-void func(int& count, std::mutex& m) {
+void func(int& count, std::mutex& m1, std::mutex& m2) {
     for(int i = 0; i < 10000; i++) {
-        std::lock_guard<std::mutex> lock(m);
+        std::lock_guard<std::mutex> lock(m1);
+        std::lock_guard<std::mutex> lock2(m2);
         count += 1;
     }
 }
 
+void func2(int& count, std::mutex& m1, std::mutex& m2) {
+    for(int i = 0; i < 10000; i++) {
+        std::lock_guard<std::mutex> lock2(m2);
+        std::lock_guard<std::mutex> lock(m1);
+        count += 1;
+    }
+}
+
+
 int main()
 {
     int count = 0;
-    std::mutex m;
+    std::mutex m1;
+    std::mutex m2;
     vector<thread> threads;
     for (int i = 0; i < 4; i++) {
-        threads.push_back(thread(func, std::ref(count), std::ref(m)));
+        threads.push_back(thread(func, std::ref(count), std::ref(m1), std::ref(m2)));
+    }
+
+    vector<thread> threads2;
+    for (int i = 0; i < 4; i++) {
+        threads2.push_back(thread(func2, std::ref(count), std::ref(m1), std::ref(m2)));
     }
 
     for (int i = 0; i < 4; i++) {
         threads[i].join();
+        threads2[i].join();
     }
 
     std::cout << count << std::endl;
