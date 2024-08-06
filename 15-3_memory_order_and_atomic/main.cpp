5f68c472985eecba4c3a7a45ad6f8473da9dfc92
trustedhost <mrchjung@korea.ac.kr>
2024-08-06 09:55:57 +0900

    15-3_memory_order_and_atomic
    thread 가 공유자원에 접근하는 과정에서 메모리 order 가 꼬이는 상태
    해결 1. mutex

--- 15-3_memory_order_and_atomic/main.cpp
+++ 15-3_memory_order_and_atomic/main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <mutex>
 
 using std::vector;
 using std::thread;
+using std::mutex;
 
-void worker(int* counter) {
+void worker(int* counter, mutex* m) {
     for(int i = 0; i < 10000; i++) {
+        m->lock();
         (*counter)++;
+        m->unlock();
     }
 }
 
 int main()
 {
     int counter = 0;
+    mutex m;
     vector<thread> threads;
     for (int i = 0; i < 4; i++) {
-        threads.push_back(thread(worker, &counter));
+        threads.push_back(thread(worker, &counter, &m));
     }
     for (int i = 0; i < 4; i++) {
         threads[i].join();
