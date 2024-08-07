05aa01c668bc9722a4d08a59db52eb7b76399272
trustedhost <mrchjung@korea.ac.kr>
2024-08-07 10:13:21 +0900

    16-1_initializer_list
    initializer_list 를 통해서 생성자를 대신할 수 있다.

---
+++ 16-1_uniform_initialization/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+
+class A {
+    int k;
+public:
+    A(int a = 0) : k(a) {}
+    int get_k() {
+        return k;
+    }
+};
+
+A func() {
+    return {4}; // 초기화 리스트를 통해서 이와 같이 리턴이 가능하다.
+}
+
+int main()
+{
+    // 문제 : () 는 함수의 인자를 의미하기도, class 의 생성자를 의미하기도 해서 중의적임.
+    // 해결책 : {} 를 통해 class 생성자를 따로 표기함. (uniform initializer)
+
+    // 유의사항 : uniform intitializer 는 일부 암시적 형변환을 거부한다.
+    // A a(3.5);
+    // A b{3.5}; // impilicit , narrowing(데이터 손실 존재) 형변환을 인정하지 않는다.
+
+    A a(1);
+    A b{2};
+    A c = {3};
+
+    std::cout << a.get_k() << b.get_k() << c.get_k()<< std::endl;
+
+
+
+
+
+
+
+    return 0;
+}
