b9becba4cf4017e5a2b9c92206c0304edef4cc83
trustedhost <mrchjung@korea.ac.kr>
2024-08-08 16:09:03 +0900

    16-3_decltype
    declval  을 통해서
    생성자가 정의되지 않은 클래스의 처리를 할 수 있다.
    = decltype 안의 식은 컴파일 타임 내에 타입이 정의될 수 있어야 한다.

--- 16-3_decltype/main.cpp
+++ 16-3_decltype/main.cpp
@@ -5,11 +5,29 @@ auto add(T t, U u) -> decltype(t + u) {
     return t + u;
 }
 
+class A {
+public:
+    A(int x) {}
+    int f() { return 1;}
+};
+
+template <typename T>
+decltype(std::declval<T>().f()) func(T t) { // std::declval 을 통해서 생성자가 정의되지 않은 Class 에 대해서 처리할 수 있다.
+    return t.f();
+}
+
 int main() {
     int a = 1;
     double b = 2.;
 
     decltype(a + b) result = add(a , b);
 
-    std::cout << result;
+    std::cout << result << '\n';
+
+
+    // decltype(A().f()) p; // decltype 내의 문장은 compile-time 에 정의될 수 있어야 한다.
+
+    A instance = A(3);
+
+    std::cout << func(instance);
 }
