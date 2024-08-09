fa8af506216c92f0e0dd90e3504597789e83a243
trustedhost <mrchjung@korea.ac.kr>
2024-08-09 13:33:33 +0900

    17-1_type_traits
    template  의 type을 "컴파일 타임"에 제한하는 방법.
     1. static_assert
     2. enable_if
     3. enable_if_t ( 2. 의 alias)

--- 17-1_type_traits_library/main.cpp
+++ 17-1_type_traits_library/main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include <type_traits>
-
-class A {
-public:
-    int i;
-    A(int i = 1) : i(i) {}
-    ~A() = default;
-};
+// 정수만 받는 template 함수 구현하기
 
 
+//1. static_assert 활용
+template <typename T>
+void only_integer_ver1(const T& t) {
+    static_assert(std::is_integral<T>::value);
+    return;
+}
+
+
+//2. enable_if 활용
+template <typename T, typename = typename std::enable_if<std::is_integral<T>::value>::type>
+void only_integer_ver2(const T& t) {
+    return ;
+}
+
+
+//3. enable_if 와 alias 활용
+template <typename T, typename = typename std::enable_if_t<std::is_integral_v<T>>>
+void only_integer_ver3(const T& t) {
+    return;
+}
+
+struct A {};
 
 int main()
 {
-    int A::* p_i = &A::i;
-
-    A a(3);
-    std::cout << a.i << '\n';
-    std::cout << a.*p_i;
+    A a;
+    only_integer_ver1(1);
+    // only_integer(a);// 컴파일 타임 에러 발생.
+    only_integer_ver3(1);
+    // only_integer_ver3(a); // 컴파일 타임 에러 발생.
+    only_integer_ver2(1);
+    // only_integer_ver2(a); // 컴파일 타임 에러 발생.
 }
