4404299ffa7813b0bfe9d6eb052d4bb30d1e2456
trustedhost <mrchjung@korea.ac.kr>
2024-08-09 14:13:05 +0900

    17-1_SFINAE
    Substitution Failure Is Not An Error.
    템플릿 유추 불가능 -> 오버로딩 후보군 제외 -> 템플릿 인스턴스마저 생성 안됨.
    Error 가 아니라
    Substitution Failure 를 발생시킴.

--- 17-1_SFINAE/main.cpp
+++ 17-1_SFINAE/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
-using namespace std;
+template <typename T, typename = decltype(std::declval<T::y>)>
+void test(typename T::x a) {
+    typename T::y b;
+}
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+template <typename T>
+void test(typename T::y b) {
+    std::cout << "T::y \n";
+}
+
+struct A {
+    using x = int;
+};
+
+struct B {
+    using y = int;
+};
+
+int main() {
+    // test<A>(11); // template ignored error 발생.
+    test<B>(11);
 }
