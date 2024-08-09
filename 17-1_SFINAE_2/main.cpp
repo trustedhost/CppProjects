8fdfbfc5a1df779c34b46af850cdf274ffe80827
trustedhost <mrchjung@korea.ac.kr>
2024-08-09 15:33:45 +0900

    17-1_SFINAE
    문제상황
    class 내의 생성자 오버로딩 상황에서
    - 좀 더 구체적인 타입 필터링이 필요한 상황.
    - is_iterator template struct 를 만들어서 template meta programming  함수 만들기.

--- 17-1_SFINAE_2/main.cpp
+++ 17-1_SFINAE_2/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <type_traits>
+#include <vector>
 
-using namespace std;
+template <typename T>
+struct is_iterator {
+private:
+    template <typename U>
+    static char test(typename std::iterator_traits<U>::iterator_category*);
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+    template <typename U>
+    static long test(...);
+public:
+    static constexpr bool value = std::is_same_v<decltype(test<T>(nullptr)), char>;
+};
+
+template <typename T>
+class vector {
+public:
+    vector(size_t num, const T& element) {
+        std::cout << element << " 를 " << num << " 개 만들기" << std::endl;
+    }
+
+
+    template <typename Iterator, typename = typename std::enable_if<is_iterator<Iterator>::value>::type >
+    vector(Iterator start, Iterator end) {
+        std::cout << "반복자를 이용한 생성자 호출" << std::endl;
+    }
+};
+
+int main() {
+    vector<int> v(10, 3);
+
+    std::vector<int> double_vec;
+    vector<int> v2(double_vec.begin(), double_vec.end());
+
 }
