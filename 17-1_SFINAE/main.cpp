f93bd8bca4fd884ac1063e8e0f993c7f55219617
trustedhost <mrchjung@korea.ac.kr>
2024-08-09 14:59:12 +0900

    17-1_SFINAE
    템플릿 메타 프로그래밍 을 통한
    오버로딩 제어 (SFINAE 심화버전)
    1. nested type T::y 가 정의된 경우
    2. T.func() 가 정의되고, 그 결과값이 int 인 경우
    2. T::y 가 정의되고, T.func() 가 "int" 로 정의된 경우

--- 17-1_SFINAE/main.cpp
+++ 17-1_SFINAE/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
-template <typename T, typename = decltype(std::declval<T::y>)>
+// 1. T::y 가 정의된 경우만 overload 를 결정.
+template <typename T, typename = decltype(std::declval<T::y>())>
 void test(typename T::x a) {
     typename T::y b;
 }
+
+// 2. T.func 가 정의되고, 그 리턴 값이 int 인 경우만 오버로딩 허용.
+template <typename T, typename = std::enable_if_t<std::is_integral_v<decltype(std::declval<T>().func())>>>
+void test() {
+    return;
+}
+
+// 3. T::z 가 정의되고, T.func 가 int 로 정의된 경우만 허용
+template <typename T,
+         typename = std::enable_if_t<
+             std::is_integral_v<decltype(std::declval<T>().func())> &&
+             std::is_same_v<decltype(std::declval<typename T::y>()) , typename T::y>
+             >>
+void test(T t) {
+    return;
+}
+
 
 template <typename T>
 void test(typename T::y b) {
@@ -12,6 +30,7 @@ void test(typename T::y b) {
 
 struct A {
     using x = int;
+    int func();
 };
 
 struct B {
@@ -21,4 +40,5 @@ struct B {
 int main() {
     // test<A>(11); // template ignored error 발생.
     test<B>(11);
+    A a;
 }
